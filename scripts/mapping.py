3be480fb56dffe49567f5cdf3d3e66b5a6242370
Anden Acitelli <a.acitelli@gmail.com>
2021-01-24 12:32:57 -0500

    node is setup for work

--- scripts/mapping.py
+++ scripts/mapping.py
@@ -13,12 +13,13 @@ currentPositions = {
 # Handles merging DOPE's output into our representation
 def dopeCallback(msg):
 
-    # TODO: 1. Merge the changes from DOPE into our representation
-    # TODO: 
+    # TODO: Merge the changes from DOPE into our representation
+    # Will need to do a little math to figure out the best way to update it.
+    # We don't want to straight-up overwrite it every time and ignore all our previous data points, but we also know new data is more accurate so we want to use it.
 
     # TODO: Publish every object's relevant points 
 
-    # Only needed because 
+    # Can be deleted once we have actual content in this function 
     pass
 
 # Kept as an example of what a callback that does some processing looks like, can be deleted once we get our feet under us 
@@ -37,7 +38,8 @@ if __name__ == '__main__':
     # TODO: Read initial positions into whatever our representation is 
 
     # TODO: Register all our subscribers and publishers 
-    # This is left over from the copy from riptide_vision, and is just an example of what pubs/subs look like 
+    # This is left over from the copy from riptide_vision, and is just an example of what pubs/subs look like.
+    # We will need at least one subscriber to parse in DOPE's output, and at least one publisher to publish our current estimate for each object of interest. 
     rospy.Subscriber("command/camera", Int8, cameraSelectionCb)
     rospy.Subscriber("darknet_ros/bounding_boxes", BoundingBoxes, bboxCb)
     cameraPub = rospy.Publisher("darknet_ros/input_image", Image, queue_size=1)
