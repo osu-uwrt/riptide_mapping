f93336b301d398dfa36201aed43084076b0896a9
Anden Acitelli <a.acitelli@gmail.com>
2021-01-26 13:36:11 -0500

    Implement all-zeroes covariance

--- scripts/Pose.py
+++ scripts/Pose.py
@@ -9,22 +9,18 @@ class CustomPose:
     def __init__(self):
         self.pose = None # Pose Type
         self.size = None # Vector3 Type
-
-        # TODO: Implement Uncertainty 
-        self.uncertainty = 0
+        self.covariance = None # float64[36] type 
 
     # TODO: This currently just updates it to the message. Implement something that gives more precedence to newer messages.
     # Takes in a new DOPE reading and updates our estimate
     # msg: Detection3D Object (http://docs.ros.org/en/lunar/api/vision_msgs/html/msg/Detection3D.html)
     def addPositionEstimate(self, msg):
 
-        # Take out the part that matters to us
-        bbox = msg.bbox # BoundingBox3D Object (http://docs.ros.org/en/lunar/api/vision_msgs/html/msg/BoundingBox3D.html)
-
         # TODO: If we don't have an estimate for this object yet, this is our new estimate!
         if (self.pose == None):
-            self.pose = bbox.center
-            self.size = bbox.size
+            self.pose = msg.results.pose.pose
+            self.covariance = msg.results.pose.covariance
+            self.size = msg.bbox.size
 
         # TODO: Otherwise, do a weighted update of our representation! Figure out how this should work. We want to give more weight to newer results, as uncertainty builds up over time.
         else:
@@ -32,8 +28,10 @@ class CustomPose:
 
     # Compiles our representation into an object that we want 
     # return: PoseWithCovarianceStamped message (http://docs.ros.org/en/melodic/api/geometry_msgs/html/msg/PoseWithCovarianceStamped.html)
+    # TODO: *Should* work, but needs tested!
     def getPoseWithCovarianceStamped(self):
         obj = PoseWithCovarianceStamped()
-        # TODO: Fill the object's pose with all the data we have access to (xyz/rpy/lwh should be enough)
+        obj.pose.pose = self.pose
+        obj.pose.covariance = self.covariance
         obj.header.stamp = rospy.get_rostime() # Time Stamp
         return obj
