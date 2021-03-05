2784395c05d1b20faf3adc314e2944980c210cc0
Anden Acitelli <a.acitelli@gmail.com>
2021-03-05 03:32:13 -0500

    Lots and lots and lots of debug changes

--- scripts/mapping.py
+++ scripts/mapping.py
@@ -4,6 +4,7 @@ import rospy
 import tf
 import yaml
 from vision_msgs.msg import Detection3DArray
+from vision_msgs.msg import Detection3D
 from geometry_msgs.msg import Pose
 from geometry_msgs.msg import PoseStamped
 from geometry_msgs.msg import PoseWithCovarianceStamped
@@ -31,22 +32,84 @@ objects = {
     "retrieve": {
         "pose": CustomPose(),
         "publisher": None
+    }, 
+    "pole": {
+        "pose": CustomPose(), 
+        "publisher": None
     }
 }
 
 # DOPE will likely give us a probability map, but it will be linked via IDs instead of names. This is how we translate.
 # We might be able to just make this an array unless the IDs we get from dope are not sequential for some reason.
 objectIDs = {
-    0 : "gate", 
-    1 : "buoy", 
-    2 : "markers", 
-    3 : "torpedoes", 
-    4 : "retrieve"
+    0 : "pole",
+    1 : "gate", 
+    2 : "buoy", 
+    3 : "markers", 
+    4 : "torpedoes", 
+    5 : "retrieve", 
 }
+
+# Handles merging DOPE's output into our representation
+# msg: Detection3D (http://docs.ros.org/en/lunar/api/vision_msgs/html/msg/Detection3D.html)
+def poleCallback(msg):
+
+    # Verify TF system is established
+    try:
+        tl.lookupTransform(worldFrame, cameraFrame, rospy.Time(0))
+    except (tf.LookupException, tf.ConnectivityException, tf.ExtrapolationException):
+        return 
+
+    # Note that we don't change the first loop to `detection in msg.detections.results` because we want the timestamp from the Detection3D object
+    # Context: This loop will run <number of objects DOPE can identify> times 
+    # `result` is of type ObjectHypothesisWithPose (http://docs.ros.org/en/lunar/api/vision_msgs/html/msg/ObjectHypothesisWithPose.html)
+    for result in msg.results: 
+
+        # NOTE: Rather than decide here if it meets our threshold for adding an estimate, the cleaner way to do it is
+        # to just pass it over regardless. addPositionEstimate will simply barely change our estimate if our certainty is low.
+
+        # Translate the ID that DOPE gives us to a name meaningful to us
+        name = objectIDs[result.id]
+
+        # DOPE's frame is the same as the camera frame, specifically the left lens of the camera.
+        # We need to convert that to the world frame, which is what is used in our mapping system 
+        # Tutorial on how this works @ http://wiki.ros.org/tf/TfUsingPython#TransformerROS_and_TransformListener
+        t = tl.getLatestCommonTime(worldFrame, cameraFrame)
+        p1 = PoseStamped()
+        p1.header.frame_id = cameraFrame
+        p1.header.stamp = t
+        p1.pose = result.pose.pose
+        convertedPos = tl.transformPose(worldFrame, p1)
+
+        # BUG: We transform a PoseStamped perfectly fine, but there's no easy way to transform the covariance, which is what gets actually fed into our system 
+
+        # poseMsg = PoseStamped()
+        # poseMsg.header = detection.header # tf transforms need a PoseStamped, not a PoseWithCovarianceStamped like we have
+        # poseMsg.pose = result.pose.pose
+        # convertedPos = tl.transformPose("world", pose)
+
+        # Merge the given position into our position for that object
+        objects[name]["pose"].addPositionEstimate(convertedPos)
+
+        # Publish that object's data out 
+        poseStamped = objects[name]["pose"].getPoseWithCovarianceStamped()
+        objects[name]["publisher"].publish(poseStamped)
+
+        # Publish /tf data for the given object 
+        pose = poseStamped.pose.pose # Get the embedded geometry_msgs/Pose (we don't need timestamp/covariance)
+        translation = (pose.position.x, pose.position.y, pose.position.z) # Needs to be a 3-tuple rather than an object
+        rotation = (pose.orientation.x, pose.orientation.y, pose.orientation.z, pose.orientation.w) # Needs to be a 4-tuple rather than an object
+        time = rospy.get_rostime()
+        child = name + "_frame"
+        parent = "world"
+        tf.TransformBroadcaster().sendTransform(translation, rotation, time, child, parent)
 
 # Handles merging DOPE's output into our representation
 # msg: Detection3DArray (http://docs.ros.org/en/lunar/api/vision_msgs/html/msg/Detection3DArray.html)
 def dopeCallback(msg):
+    pass
+
+    '''
 
     # Context: This loop will run <number of different objects DOPE thinks it sees on screen> times
     # `detection` is of type Detection3D (http://docs.ros.org/en/lunar/api/vision_msgs/html/msg/Detection3D.html)
@@ -68,17 +131,19 @@ def dopeCallback(msg):
             # We need to convert that to the world frame, which is what is used in our mapping system 
             # Tutorial on how this works @ http://wiki.ros.org/tf/TfUsingPython#TransformerROS_and_TransformListener
             worldFrame = "/world"
-            cameraFrame = "/{}/stereo/left_link".format(rospy.get_namespace())
+            cameraFrame = "{}stereo/left_link".format(rospy.get_namespace())
             convertedPos = None
-            if tf.frameExists(worldFrame) and tf.frameExists(cameraFrame):
+            if transformer.frameExists(worldFrame) and transformer.frameExists(cameraFrame):
                 t = tl.getLatestCommonTime(worldFrame, cameraFrame)
+                rospy.logwarn_throttle(1, "t: " + str(t))
                 p1 = PoseStamped()
                 p1.header.frame_id = cameraFrame
                 p1.header.stamp = t
                 p1.pose = result.pose.pose
+                rospy.logwarn_throttle(1, "p1: " + str(p1))
                 convertedPos = tl.transformPose(worldFrame, p1)
             else:
-                rospy.logfatal("ERROR: Mapping was unable to find /world and /{}/stereo/left_link frames!".format(rospy.get_namespace()))
+                rospy.logerr_throttle(1, "ERROR: Mapping was unable to find /world and {}stereo/left_link frames!".format(rospy.get_namespace()))
 
 
             # poseMsg = PoseStamped()
@@ -101,6 +166,7 @@ def dopeCallback(msg):
             child = name + "_frame"
             parent = "world"
             tf.TransformBroadcaster().sendTransform(translation, rotation, time, child, parent)
+'''
 
 
 # Takes in an object and position data to produce an initial pose estimate 
@@ -145,7 +211,10 @@ if __name__ == '__main__':
 
     rospy.init_node("mapping")
 
+    # "class" variables 
     tl = tf.TransformListener()
+    worldFrame = "/world"
+    cameraFrame = "{}stereo/left_link".format(rospy.get_namespace())
 
     # Initial object positions loaded from positions.yaml
 
@@ -157,13 +226,13 @@ if __name__ == '__main__':
         newPose = initialObjectPose(object, initial_positions)
         # Set this objects pose to the new one that was just created
         objects[object]['pose'] = newPose
-
         
     # Subscribers
     rospy.Subscriber("/dope/detected_objects", Detection3DArray, dopeCallback) # DOPE's information 
+    rospy.Subscriber("pole_detection", Detection3D, poleCallback)
 
     # Publishers
     for field in objects:
-        objects[field]["publisher"] = rospy.Publisher("/mapping/" + field, PoseWithCovarianceStamped, queue_size=1)
+        objects[field]["publisher"] = rospy.Publisher("mapping/" + field, PoseWithCovarianceStamped, queue_size=1)
 
     rospy.spin()
